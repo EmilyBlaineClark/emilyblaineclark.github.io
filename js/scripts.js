0a5fd293e5ad97c14d8b79c3bfbc508b675534a3
Emily Clark <emilyblaineclark@gmail.com>
2017-08-19 15:50:42 -0400

    race condition on contact form.

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,9 @@
     parent: jQuery('.page__contact-html')
   });
 
-  $(window).trigger('resize');
+  setTimeout(function(){
+    $(window).trigger('resize');
+  }, 500);
 
   $("img.scale").imageScale({
     rescaleOnResize: true,
