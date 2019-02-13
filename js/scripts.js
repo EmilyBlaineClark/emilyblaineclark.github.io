dbb6902abeeb54fabb7cf397fe36c71ec6e5c071
Michael VanDyke <michaeljamesvandyke@gmail.com>
2019-02-12 19:13:05 -0500

    [homepage] rework work page

--- js/scripts.js
+++ js/scripts.js
@@ -118,6 +118,7 @@
     jQuery(".grid").masonry({
       itemSelector: '.grid-item',
       columnWidth: '.grid-sizer',
+      // gutter: '.gutter-sizer',
       percentPosition: true,
       horizontalOrder: true
     });
